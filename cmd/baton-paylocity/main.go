eb4d12e71cde209bf24c60d493cbd8be659aee25
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-12-20 14:24:57 -0600

    Add placeholder values when creating connector

--- cmd/baton-paylocity/main.go
+++ cmd/baton-paylocity/main.go
@@ -5,13 +5,13 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/conductorone/baton-paylocity/pkg/connector"
 	"github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
 	"github.com/conductorone/baton-sdk/pkg/field"
 	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 	"github.com/spf13/viper"
-	"github.com/conductorone/baton-paylocity/pkg/connector"
 	"go.uber.org/zap"
 )
 
@@ -48,7 +48,8 @@ func getConnector(ctx context.Context, v *viper.Viper) (types.ConnectorServer, e
 		return nil, err
 	}
 
-	cb, err := connector.New(ctx)
+	// FIXME(shackra): fill the input arguments
+	cb, err := connector.New(ctx, "", "", "", "")
 	if err != nil {
 		l.Error("error creating connector", zap.Error(err))
 		return nil, err
