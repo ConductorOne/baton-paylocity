483be2e9d79881a1401ce75257d0aadc0f6d61d7
Mateo Vespasiano <mateo.vespasiano@conductorone.com>
2025-08-25 16:41:52 -0300

    Add Alejandro's suggestion

--- pkg/client/client_interface.go
+++ pkg/client/client_interface.go
@@ -11,19 +11,3 @@ type ClientService interface {
 	ListPositionCodes(ctx context.Context, options PageOptions) ([]*Position, string, *v2.RateLimitDescription, error)
 	GetUserById(ctx context.Context, userId string) (*User, *v2.RateLimitDescription, error)
 }
-
-type ClientServiceImpl struct {
-	client *PaylocityClient
-}
-
-func (m *ClientServiceImpl) ListEmployees(ctx context.Context, options PageOptions) ([]*User, string, *v2.RateLimitDescription, error) {
-	return m.client.ListEmployees(ctx, options)
-}
-
-func (m *ClientServiceImpl) ListPositionCodes(ctx context.Context, options PageOptions) ([]*Position, string, *v2.RateLimitDescription, error) {
-	return m.client.ListPositionCodes(ctx, options)
-}
-
-func (m *ClientServiceImpl) GetUserById(ctx context.Context, userId string) (*User, *v2.RateLimitDescription, error) {
-	return m.client.GetUserById(ctx, userId)
-}
